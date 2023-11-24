18cdf86147224011c00c8d789e71b36a974eefc0
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-11-23 19:12:45 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'; // Create a CSS file for styling
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
 
 const App = () => {
   const [loadingBars, setLoadingBars] = useState(0);
   const [showTerminal, setShowTerminal] = useState(false);
   const [showText, setShowText] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [terminalMessages, setTerminalMessages] = useState([]);
+  const inputRef = useRef(null);
 
   useEffect(() => {
-    // Simulate loading screen
     const loadingInterval = setInterval(() => {
       if (loadingBars < 100) {
         setLoadingBars(loadingBars + 10);
       } else {
         clearInterval(loadingInterval);
         setShowTerminal(true);
+        inputRef.current.focus();
       }
     }, 500);
 
-    // Simulate glitch effect
     const glitchTimeout = setTimeout(() => {
       setShowTerminal(false);
       setTimeout(() => {
@@ -25,7 +27,6 @@ const App = () => {
       }, 200);
     }, 3000);
 
-    // Clean up intervals and timeouts
     return () => {
       clearInterval(loadingInterval);
       clearTimeout(glitchTimeout);
@@ -33,7 +34,6 @@ const App = () => {
   }, [loadingBars]);
 
   useEffect(() => {
-    // Typewriter animation for text
     if (showTerminal) {
       const textTimeout = setTimeout(() => {
         setShowText(true);
@@ -44,6 +44,36 @@ const App = () => {
       };
     }
   }, [showTerminal]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleEnterPress = (e) => {
+    if (e.key === 'Enter') {
+      handleCommand();
+    }
+  };
+
+  const handleCommand = () => {
+  const command = inputValue.trim().toLowerCase();
+
+  // Combine user command and system response before updating the terminal messages
+  const newMessages = [...terminalMessages, `$ ${inputValue}`, ''];
+
+  if (command === 'help') {
+    // Display the system response in the terminal
+    newMessages.push('hello user');
+  } else {
+    // Display a default response for unrecognized commands
+    newMessages.push(`Command not recognized: ${inputValue}`);
+  }
+
+  // Update terminal messages with both user command and system response
+  setTerminalMessages(newMessages);
+
+  setInputValue('');
+};
 
   return (
     <div className="app">
@@ -60,6 +90,19 @@ const App = () => {
               <div className="typewriter">
                 <div>Aanshi Patel</div>
                 <div>$&nbsp;</div>
+              </div>
+              {terminalMessages.map((message, index) => (
+                <div key={index}>{message}</div>
+              ))}
+              <div className="input-container">
+                <div>$&nbsp;</div>
+                <input
+                  ref={inputRef}
+                  type="text"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  onKeyPress={handleEnterPress}
+                />
               </div>
             </div>
           )}
